c44e6186ab770eb9c6ea4f5d99886002b64aaf8f
Tyler Young <tyler.young@stirlinglabs.com>
2022-03-30 10:05:40 -0400

    update tests to play nicer on failure

--- StirlingLabs.Utilities.Collections.Tests/AsyncProducerConsumerCollectionTests.cs
+++ StirlingLabs.Utilities.Collections.Tests/AsyncProducerConsumerCollectionTests.cs
@@ -24,101 +24,116 @@ public class AsyncProducerConsumerCollectionTests
     [SuppressMessage("ReSharper", "AccessToDisposedClosure")]
     public async Task GeneralQueueOperations()
     {
-        var objects = new object[8000];
+        using var cts = new CancellationTokenSource();
+        Thread? qt1 = null;
+        Thread? qt2 = null;
+        try
+        {
 
-        for (var o = 0; o < 1000; ++o) objects[o] = new();
+            var objects = new object[8000];
 
-        using var q = new AsyncProducerConsumerCollection<object>(objects);
+            for (var o = 0; o < 1000; ++o) objects[o] = new();
 
-        q.IsEmpty.Should().BeFalse();
-        var initCount = objects.Length;
-        q.Count.Should().Be(initCount);
+            using var q = new AsyncProducerConsumerCollection<object>(objects);
 
-        using var mre1 = new ManualResetEventSlim();
+            q.IsEmpty.Should().BeFalse();
+            var initCount = objects.Length;
+            q.Count.Should().Be(initCount);
 
-        var qt1 = RunThread(() => {
-            mre1.Wait();
-            mre1.Dispose();
-            q.TryAdd(objects.First());
-            q.TryAddRange(objects.Skip(1));
-        });
+            using var mre1 = new ManualResetEventSlim();
 
-        using var mre2 = new ManualResetEventSlim();
+            qt1 = RunThread(() => {
+                try { mre1.Wait(cts.Token); }
+                catch (OperationCanceledException) { }
+                mre1.Dispose();
+                if (cts.IsCancellationRequested) return;
+                q.TryAdd(objects.First());
+                q.TryAddRange(objects.Skip(1));
+            });
 
-        var qt2 = RunThread(() => {
-            mre2.Wait();
-            mre2.Dispose();
-            q.TryAddRange(objects);
-            q.CompleteAdding();
-        });
+            using var mre2 = new ManualResetEventSlim();
 
-        var i = 0;
-        var c = q.GetConsumer();
-        c.Should().NotBeNull();
+            qt2 = RunThread(() => {
+                try { mre2.Wait(cts.Token); }
+                catch (OperationCanceledException) { }
+                mre2.Dispose();
+                if (cts.IsCancellationRequested) return;
+                q.TryAddRange(objects);
+                q.CompleteAdding();
+            });
 
-        c.Equals(c).Should().BeTrue();
+            var i = 0;
+            var c = q.GetConsumer();
+            c.Should().NotBeNull();
 
-        c.Equals((object)c).Should().BeTrue();
+            c.Equals(c).Should().BeTrue();
 
-        c.GetHashCode().Should().Be(q.GetHashCode());
+            c.Equals((object)c).Should().BeTrue();
+
+            c.GetHashCode().Should().Be(q.GetHashCode());
 
 #pragma warning disable 1718 // intentional, calls overloaded operator
-        // ReSharper disable once EqualExpressionComparison
-        (c == c).Should().BeTrue();
+            // ReSharper disable once EqualExpressionComparison
+            (c == c).Should().BeTrue();
 
-        // ReSharper disable once EqualExpressionComparison
-        (c != c).Should().BeFalse();
+            // ReSharper disable once EqualExpressionComparison
+            (c != c).Should().BeFalse();
 #pragma warning restore 1718
 
-        foreach (var item in c)
-        {
-            item.Should().Be(objects[i++]);
-            q.IsAddingCompleted.Should().BeFalse();
-            c.IsCompleted.Should().BeFalse();
-            if (i >= initCount) break;
-            c.IsEmpty.Should().BeFalse();
-            //q.Count.Should().Be(initCount - i);
+            foreach (var item in c)
+            {
+                item.Should().Be(objects[i++]);
+                q.IsAddingCompleted.Should().BeFalse();
+                c.IsCompleted.Should().BeFalse();
+                if (i >= initCount) break;
+                c.IsEmpty.Should().BeFalse();
+                //q.Count.Should().Be(initCount - i);
+            }
+            i.Should().Be(initCount);
+            q.IsEmpty.Should().BeTrue();
+            q.Count.Should().Be(0);
+
+            mre1.Set();
+            i = 0;
+
+            await foreach (var item in c)
+            {
+                item.Should().Be(objects[i++]);
+                q.IsAddingCompleted.Should().BeFalse();
+                c.IsCompleted.Should().BeFalse();
+                if (i >= initCount) break;
+                c.IsEmpty.Should().BeFalse();
+                //q.Count.Should().Be(initCount - i);
+            }
+            i.Should().Be(initCount);
+            c.IsEmpty.Should().BeTrue();
+            q.Count.Should().Be(0);
+
+            mre2.Set();
+            i = 0;
+            await foreach (var item in c)
+            {
+                item.Should().Be(objects[i++]);
+                if (i >= initCount) break;
+                c.IsCompleted.Should().BeFalse();
+                c.IsEmpty.Should().BeFalse();
+                //q.Count.Should().Be(initCount - i);
+            }
+
+            q.IsAddingCompleted.Should().BeTrue();
+            q.IsCompleted.Should().BeTrue();
+
+            i.Should().Be(initCount);
+
+            q.IsEmpty.Should().BeTrue();
+            q.Count.Should().Be(0);
         }
-        i.Should().Be(initCount);
-        q.IsEmpty.Should().BeTrue();
-        q.Count.Should().Be(0);
-
-        mre1.Set();
-        i = 0;
-
-        await foreach (var item in c)
+        finally
         {
-            item.Should().Be(objects[i++]);
-            q.IsAddingCompleted.Should().BeFalse();
-            c.IsCompleted.Should().BeFalse();
-            if (i >= initCount) break;
-            c.IsEmpty.Should().BeFalse();
-            //q.Count.Should().Be(initCount - i);
+            cts.CancelAfter(125);
+
+            qt1?.Join();
+            qt2?.Join();
         }
-        i.Should().Be(initCount);
-        c.IsEmpty.Should().BeTrue();
-        q.Count.Should().Be(0);
-
-        mre2.Set();
-        i = 0;
-        await foreach (var item in c)
-        {
-            item.Should().Be(objects[i++]);
-            if (i >= initCount) break;
-            c.IsCompleted.Should().BeFalse();
-            c.IsEmpty.Should().BeFalse();
-            //q.Count.Should().Be(initCount - i);
-        }
-
-        q.IsAddingCompleted.Should().BeTrue();
-        q.IsCompleted.Should().BeTrue();
-
-        i.Should().Be(initCount);
-
-        q.IsEmpty.Should().BeTrue();
-        q.Count.Should().Be(0);
-
-        qt1.Join();
-        qt2.Join();
     }
 }
